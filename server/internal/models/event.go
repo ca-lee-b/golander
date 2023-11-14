2d119c982a9639f66aa5b3765201e082b5db125c
a-pillow <kaluhblee@gmail.com>
2023-11-13 19:46:53 -0600

    init models

---
+++ server/internal/models/event.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Event struct {
+	Id              int         `json:"id"`
+	Title           string      `json:"title"`
+	Owner_id        string      `json:"owner_id"`
+	Created_at      time.Time   `json:"created_at"`
+	Available_dates []time.Time `json:"available_dates"`
+}
